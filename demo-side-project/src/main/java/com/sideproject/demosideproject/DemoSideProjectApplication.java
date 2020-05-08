223eecd1924da6e25a2d10ef0f0a34830a413839
HernikV <henrik.vendel@outlook.com>
2020-05-08 14:52:27 +0200

    Sideproject start

---
+++ demo-side-project/src/main/java/com/sideproject/demosideproject/DemoSideProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.demosideproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSideProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSideProjectApplication.class, args);
+	}
+
+}
